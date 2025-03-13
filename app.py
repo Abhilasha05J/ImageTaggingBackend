c532df7e6093c1acb91c833c2fa534327161a2db
unknown <abhilasha05j@gmail.com>
2025-03-13 14:19:07 +0530

    first commit

--- app.py
+++ app.py
@@ -213,4 +213,5 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0",  port=5000)
+    
